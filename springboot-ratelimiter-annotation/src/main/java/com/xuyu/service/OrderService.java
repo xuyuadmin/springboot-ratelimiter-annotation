b4b4b14b3059a5d32e828edc9b65162d20e4807d
xuyuadmin <860520677@qq.com>
2018-10-07 11:26:13 +0800

    springboot-ratelimiter-annotation
    springboot-ratelimiter-annotation

---
+++ springboot-ratelimiter-annotation/src/main/java/com/xuyu/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.xuyu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+	public boolean addOrder() {
+		System.out.println("db....正在操作订单表数据库...");
+		return true;
+	}
+
+}
